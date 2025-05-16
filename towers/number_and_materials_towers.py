6fb437a01645bec5aef4f3112be04402bd22633b
rrolph <rebecca.fuchs@nrel.gov>
2025-05-16 15:35:26 -0600

    finished calculating number of towers per mile for each scenario and voltage

--- towers/number_and_materials_towers.py
+++ towers/number_and_materials_towers.py
@@ -9,24 +9,25 @@ import matplotlib.pyplot as plt
 
 
 projpath = os.path.dirname(os.getcwd())
-datapath  = os.path.join(projpath,'outputs/')
-figpath = os.path.join(projpath,'plots/')
+datapath  = os.path.join(projpath,'towers','data_conductor_lengths')
+figpath = os.path.join(projpath,'plots')
 
 #%% Constants
-KVS = [69, 115, 138, 161, 230, 345, 500, 765]
+kv_values = [230, 345, 500, 765]
+circuit = 'double'
 
 #%% Import conductor lengths 
 #use_avg = False # Don't use average number of cables per bundle.   These files have been generated from find_cable_lengths...py
 #hvac_data = pd.read_pickle(datapath+'HVAC_Scenario_HVAC_lines' + str(use_avg) + '_avg_num_bundles.pkl')
 #mthvdc_dc_data = pd.read_pickle(datapath+'MTHVDC_Scenario_HVDC_lines' + str(use_avg) + '_avg_num_bundles.pkl')
 #mthvdc_ac_data = pd.read_pickle(datapath+'MTHVDC_Scenario_HVAC_lines' + str(use_avg) + '_avg_num_bundles.pkl')
-
+## Might replace above lines with Ram's numbers
 
 
 #%% Functions
 
+# Inputs include assumptions about percentage of each type of structure (e.g. percent_Tangent = 0.7 assumes 70% of all structures are tangent structures)
 def get_number_structures(
-        kv=500,
         AC_or_DC_conductor='AC',
         AC_or_DC_scenario='AC',
         percent_Tangent = 0.7,
@@ -35,173 +36,92 @@ def get_number_structures(
         percent_AngledDeadend = 0.1
     ):
 
-    circuit_number = [
-        'single',
-        'double',
-    ]
-    dictin_structure = {
-        bus: pd.read_csv(
-            os.path.join(
-                datapath,
-                f'tower_{AC_or_DC_conductor}_conductor_{circuit}_circuit.csv'
-            ),
-            index_col=0,
-        )
-        for circuit in circuit_number
+    percent_values = {
+        'percent_Tangent': percent_Tangent,
+        'percent_RunningAngle': percent_RunningAngle,
+        'percent_NonAngledDeadend': percent_NonAngledDeadend,
+        'percent_AngledDeadend': percent_AngledDeadend
     }
-    struct_specs = pd.concat(dictin_structure, axis=0)[str(kv)].unstack(0)
+
+    print(AC_or_DC_conductor)
+
+    print(f'tower_{AC_or_DC_conductor}_conductor_{circuit}_circuit.csv')
+
+
+    df_structs = pd.read_csv(os.path.join(datapath,f'tower_{AC_or_DC_conductor}_conductor_{circuit}_circuit.csv'), header=None)
+    df_structs.columns = ['kv'] + df_structs.iloc[0, 1:].astype(float).astype(int).tolist()
+    df_structs = df_structs.iloc[1:,:]
+    df_structs = df_structs.set_index('kv').T
 
     ## Read in how many miles of each type of conductor
     miles_of_conductor = pd.read_csv(
         os.path.join(datapath, f'miles_conductor_{AC_or_DC_scenario}_scenario_{AC_or_DC_conductor}_lines.csv')
     ).squeeze()
+    print(datapath, f'miles_conductor_{AC_or_DC_scenario}_scenario_{AC_or_DC_conductor}_lines.csv')
 
     structure_types = ['Tangent', 'RunningAngle', 'NonAngledDeadend', 'AngledDeadend']
-    df_number_of_structures = pd.DataFrame(columns=structure_types)
-    
+    df_number_of_structures = pd.DataFrame(index=miles_of_conductor['kV'],columns=structure_types)
+
+    # Find number of structures by type
     for struct in structure_types:
-        number_of_structures[struct] = struct_specs.loc[f'{struct}_structures_per_mile'] * miles_of_conductor[miles_of_conductor['kV'] == kv]['miles'] * f'percent_{struct}'
+        for kv in df_structs.index:
+            df_number_of_structures.loc[kv,struct] = df_structs.loc[kv, f'{struct}_structures_per_mile'] * miles_of_conductor[miles_of_conductor['kV'] == kv]['miles'].iloc[0] * percent_values[f'percent_{struct}']
 
-
+    # !!!!!!! Can add here similar to the for loop above, something like..
+    #for struct in structure_types:
+    #    for kv in df_structs.index:
+    #        df_material_for_structures.loc[kv,struct] = df_materials.loc[struct, f'{struct}_kg_al_per_struct'] * df_number_of_structures.loc[kv,struct]
+    
     return df_number_of_structures
 
 
 ### !! Can add in another def here to calculate the total weight of al and stl using the df_number_of_structures above
 
+
+
+
+
+
 #%%### Procedure
 # if __name__ == '__main__':  ## commented because of vscode settings. change later.
-#%% Imports
-import matplotlib.pyplot as plt
-import site
-
-# %%
-### Call functions
-# List of kv values to loop through
-kv_values = [69,115,138,161,230,345,500,765] 
-
 
 # Constants for other parameters
-substation_option = 'new'  # 'upgrade' for 1 or 2 positions, 'new', for 4 or 6 positions
-num_positions = 4
-landtype = 'light_veg' # light_veg, forest, wetland
-# 'ring', 'breaker_and_half', 'double_breaker'  ... note the code produces data for all 3
-# bus types given the constants above... can be reworked to be more efficient if desired
-bus_type_for_plot = 'breaker_and_half'
-fig_filename = os.path.join(
-    figpath, f"{substation_option}_{bus_type_for_plot}_{num_positions}positions_{landtype}.png"
+AC_or_DC_conductor='DC'
+AC_or_DC_scenario='DC'
+percent_Tangent = 0.7
+percent_RunningAngle = 0.1
+percent_NonAngledDeadend = 0.1
+percent_AngledDeadend = 0.1
+
+
+df_number_struct = get_number_structures(
+    AC_or_DC_conductor=AC_or_DC_conductor,
+    AC_or_DC_scenario=AC_or_DC_scenario,
+    percent_Tangent = percent_Tangent,
+    percent_RunningAngle = percent_RunningAngle,
+    percent_NonAngledDeadend = percent_NonAngledDeadend,
+    percent_AngledDeadend = percent_AngledDeadend
 )
 
 
-# List to collect plot data for all kv values
-all_plot_data = []
 
-# Loop through each kv value
-for kv in kv_values:
-    # Access road and terrain costs
-    df_access_and_terrain = get_land_terrain_cost(
-        kv=kv,
-        substation_option=substation_option,
-        num_positions=num_positions,
-        landtype=landtype
-    )
+#%% Create a stacked bar plot
+df_number_thousands_of_structures = df_number_struct/1e3
+ax = df_number_thousands_of_structures.plot(kind='bar', stacked=True, figsize=(10, 6))
 
-    # Cable component costs
-    cable_costs_df = get_cable_costs(
-        kv=kv,
-        substation_option=substation_option,
-        num_positions=num_positions
-    )
+# Add labels and title
+#plt.rcParams.update({'font.size': 18})
+plt.xlabel('kV',fontsize=24)
+plt.ylabel('Number of Structures (x1000)',fontsize=18)
+plt.title(f'{AC_or_DC_scenario} scenario, {AC_or_DC_conductor} conductor, {circuit} circuit',fontsize=18)
+ax.set_ylim([0,100])
+plt.legend(title='Structure Type', bbox_to_anchor=(1.05, 1), loc='upper left', prop={'size': 14})
 
-    # Other component costs
-    components = [
-        ('circuit_breaker_unit_costs.csv', 'circuit_breaker'),
-        ('disconnect_switch_unit_costs.csv', 'disconnect_switches'),
-        ('bus_unit_costs.csv', 'bus_support'),
-        ('voltage_transformer_unit_costs.csv', 'voltage_transformers'),
-        ('control_enclosure_unit_costs.csv', 'control_enclosure'),
-        ('relay_panel_costs.csv', 'relay_panel'),
-        ('deadend_angled_structure_costs.csv', 'deadend_struct')
-    ]
+# Show the plot
+plt.tight_layout()
+plt.show()
 
-    # Dictionary to store the costs
-    component_costs_dict = {}
-
-    # Loop through each component to calculate costs
-    for csv_file, component_name in components:
-        component_costs_dict[component_name] = component_costs(
-            kv=kv,
-            substation_option=substation_option,
-            num_positions=num_positions,
-            csv=csv_file,
-            component_name=component_name
-        )
-
-    # Transforming the data into a DataFrame
-    rows = []
-    for cost_type, series in component_costs_dict.items():
-        for bus_type, cost in series.items():
-            rows.append({'bus_type': bus_type, 'cost': cost, 'cost_type': cost_type})
-
-    component_costs_df = pd.DataFrame(rows)
-
-    ### Combine dataframes
-    df = pd.concat([df_access_and_terrain, cable_costs_df, component_costs_df])
-
-    ### Add common costs
-    pivot_df = df.pivot_table(
-        index='bus_type', columns='cost_type', values='cost', aggfunc='sum').fillna(0)
-    total_cost_by_bustype = pivot_df.copy()
-    total_cost_by_bustype = total_cost_by_bustype.sum(axis=1)
-    total_mult = get_common_cost_mult()
-    pivot_df['softcost'] = total_cost_by_bustype * (total_mult - 1)
-
-    ### Prepare for plotting
-    plot_df = pivot_df.copy()
-    plot_df = plot_df / 1e6  # Convert to millions
-    plot_df['kv'] = kv  # Add kv to the plot DataFrame
-
-    # Append the current plot data to the list
-    all_plot_data.append(plot_df)
+# %%  Calculate the amount of steel and aluminum required for each voltage
 
 
 
-## Get validation costs for each kv
-validation_costs = []
-for kv in kv_values:
-    MISO_validation_costs = get_validation_cost(
-        kv=kv,
-        substation_option=substation_option,
-        num_positions=num_positions,
-        bus_type_for_plot=bus_type_for_plot
-    )
-    validation_costs.append(MISO_validation_costs)
-
-validation_to_plot = pd.DataFrame(validation_costs)
-
-
-# Concatenate all plot data into a single DataFrame
-final_plot_df = pd.concat(all_plot_data)
-
-# Run and save plot
-stacked_bar(
-    final_plot_df=final_plot_df,
-    MISO_validation_costs=validation_to_plot[bus_type_for_plot],
-    fig_filename=fig_filename,
-)
-
-
-#### !!! NOTES BELOW TO BE DISCUSSED !!! #####
-
-#  Table 2.3 -6 (current transformer)
-# !! Verify with MISO these are not included in costs
-
-#  Table 2.3 -8 (Grid supporting devices unit costs)
-# !! Verify with MISO these are not included in validation costs
-
-#  Table 2.3 -7 (Power transformer)  # !! Not all substations need power transformers
-#     If we use Table 3.1 - 5 to get number of MVAs,
-#  .. then the costs are much higher than the validation values in Table 4.2-1.
-#  .. For example, Table 3.1 -5  gives 2598 MVA for 500kV/500kV.
-#  .. when multiplied by 13784 $/MVA from Table 2.3 -7,
-#  .. that is already 35 $M, order of magnitude higher than validation value in Table 4.1-1.
-# %%

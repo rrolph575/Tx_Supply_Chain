69fbf05b50ddbcc1b7fcdd1fed61b7c8187e5b2a
Becca Rolph <rebecca.rolph@nrel.gov>
2025-11-05 17:01:28 -0700

    Added total material plot

--- paper2_demand_and_supply/material_demand_by_component.py
+++ paper2_demand_and_supply/material_demand_by_component.py
@@ -1,83 +1,42 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.patches import Patch
+width = 0.35  # width of each "half" bar (MT or AC)
+x = np.arange(len(materials))
 
-def plot_material_scenarios(material, excel_file='Grid_RING_inputs_v2.xlsx'):
-    """
-    Plot a stacked bar chart of material required by year and technology,
-    comparing two scenarios: MT and AC.
-    MT = solid bars, AC = hatched bars. Colors indicate technology.
-    Legend shows technologies by color, scenario by hatched/solid white boxes.
-    """
+fig, ax = plt.subplots(figsize=(10, 6))  # slightly smaller width
 
-    years = [str(y) for y in range(2024, 2036)]
+for i, material in enumerate(materials):
+    tech_mt = read_material_data(material, 'MT', excel_file)
+    tech_ac = read_material_data(material, 'AC', excel_file)
 
-    # Read MT and AC data
-    df_mt = pd.read_excel(excel_file, sheet_name=f'MTGrid_2024MidCase_{material}', skiprows=6)
-    df_ac = pd.read_excel(excel_file, sheet_name=f'ACGrid_2024MidCase_{material}', skiprows=6)
+    mt_sum = tech_mt.sum(axis=1) / 1e6
+    ac_sum = tech_ac.sum(axis=1) / 1e6
 
-    tech_mt = df_mt.groupby('Tech')[years].sum()
-    tech_ac = df_ac.groupby('Tech')[years].sum()
+    mt_bottom = 0
+    ac_bottom = 0
 
-    # Union of all technologies
-    all_techs = tech_mt.index.union(tech_ac.index)
+    for tech in all_techs:
+        mt_value = mt_sum.get(tech, 0)
+        ac_value = ac_sum.get(tech, 0)
 
-    # Ensure both DataFrames have all techs (fill missing with zeros)
-    tech_mt = tech_mt.reindex(all_techs, fill_value=0)
-    tech_ac = tech_ac.reindex(all_techs, fill_value=0)
+        # MT solid
+        ax.bar(i - width/2, mt_value, width, bottom=mt_bottom, color=tech_colors[tech])
+        mt_bottom += mt_value
 
-    # Convert to millions of metric tons
-    tech_mt = tech_mt / 1e6
-    tech_ac = tech_ac / 1e6
-    ylabel = f'{material} required \n(Millions of metric tons)'
+        # AC hatched
+        ax.bar(i + width/2, ac_value, width, bottom=ac_bottom, color=tech_colors[tech], hatch='//')
+        ac_bottom += ac_value
 
-    plt.rcParams.update({
-        'font.size': 18,
-        'axes.titlesize': 18,
-        'axes.labelsize': 18,
-        'xtick.labelsize': 16,
-        'ytick.labelsize': 16,
-        'legend.fontsize': 12,
-    })
-    plt.figure(figsize=(12, 6))
+# Reduce x-axis spacing by adjusting limits
+ax.set_xticks(x)
+ax.set_xticklabels(materials)
+ax.set_xlim(-0.5, len(materials)-0.5)  # tighter limits
 
-    # Stacked bar plot with solid MT, hatched AC
-    x = np.arange(len(years))
-    width = 0.4
-    colors = plt.cm.tab20.colors  # up to 20 distinct colors
+# Legends same as before
+tech_handles = [mpatches.Patch(facecolor=tech_colors[tech], label=tech) for tech in all_techs]
+tech_legend = ax.legend(handles=tech_handles, bbox_to_anchor=(1, 1), loc='upper left', frameon=True)
+mt_patch = mpatches.Patch(facecolor='white', edgecolor='black', label='MT')
+ac_patch = mpatches.Patch(facecolor='white', edgecolor='black', hatch='//', label='AC')
+scenario_legend = ax.legend(handles=[mt_patch, ac_patch], bbox_to_anchor=(1, 0.4), loc='upper left')
+ax.add_artist(tech_legend)
 
-    # MT bars (solid)
-    bottom_mt = np.zeros(len(years))
-    for i, tech in enumerate(all_techs):
-        plt.bar(x - width/2, tech_mt.loc[tech], width=width, bottom=bottom_mt,
-                color=colors[i % len(colors)], label=tech)
-        bottom_mt += tech_mt.loc[tech]
-
-    # AC bars (hatched)
-    bottom_ac = np.zeros(len(years))
-    for i, tech in enumerate(all_techs):
-        plt.bar(x + width/2, tech_ac.loc[tech], width=width, bottom=bottom_ac,
-                color=colors[i % len(colors)], hatch='//')
-        bottom_ac += tech_ac.loc[tech]
-
-    plt.xticks(x, years, rotation=90)
-    plt.ylabel(ylabel)
-    
-    if material == 'Steel':
-        plt.ylim(0, 0.7)
-
-    # Create custom legend
-    handles_tech = [Patch(facecolor=colors[i % len(colors)], label=tech) for i, tech in enumerate(all_techs)]
-    handles_scenario = [Patch(facecolor='white', edgecolor='black', hatch='', label='MT'),
-                        Patch(facecolor='white', edgecolor='black', hatch='//', label='AC')]
-    plt.legend(handles=handles_tech + handles_scenario, bbox_to_anchor=(1.05, 1), loc='upper left')
-
-    plt.tight_layout()
-    plt.show()
-
-
-# Procedure
-materials = ['Al', 'Cu', 'Steel']  # GOES removed
-for material in materials:
-    plot_material_scenarios(material)
+plt.tight_layout(rect=[0, 0, 0.8, 1])
+plt.show()
